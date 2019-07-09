7dd38bc365c45ef84f951107194b149ab34771de
SimonStr <mymail795@gmail.com>
2019-07-09 14:39:04 +0300

    create answer form

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -3,15 +3,28 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AnswerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('body', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Your answer may not be blank.'
+                    ])
+                ],
+                'label' => 'Your Answer'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Post Answer'
+            ])
         ;
     }
 
