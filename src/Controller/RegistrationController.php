575227bb61d6fd3be8bbc8b03348de63353b732d
SimonStr <mymail795@gmail.com>
2019-07-23 11:35:47 +0300

    update registration, set user data from post request

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,49 +5,92 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\UserAuthenticator;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="app_register")
+     * @Route("/register", name="app_register", methods={"POST"})
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthenticator $authenticator): Response
+    public function register(ObjectManager $om, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+//    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthenticator $authenticator): Response
     {
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+//        Old registration form
+//        $user = new User();
+//        $form = $this->createForm(RegistrationFormType::class, $user);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            // encode the plain password
+//            $user->setPassword(
+//                $passwordEncoder->encodePassword(
+//                    $user,
+//                    $form->get('plainPassword')->getData()
+//                )
+//            );
+//
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $entityManager->persist($user);
+//            $entityManager->flush();
+//
+//            // do anything else you need here, like send an email
+//
+//            return $guardHandler->authenticateUserAndHandleSuccess(
+//                $user,
+//                $request,
+//                $authenticator,
+//                'main' // firewall name in security.yaml
+//            );
+//        }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+//        return $this->render('registration/register.html.twig', [
+//            'registrationForm' => $form->createView(),
+//        ]);
+//        ===================================
+        $errors = [];
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+        $email = $request->request->get("email");
+        $password = $request->request->get("password");
+        $password_confirmation = $request->request->get("password_confirmation");
 
-            // do anything else you need here, like send an email
-
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $user,
-                $request,
-                $authenticator,
-                'main' // firewall name in security.yaml
-            );
+        if (strlen($password) < 6) {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+        if ($password != $password_confirmation) {
+            $errors[] = "Password does not match the password confirmation.";
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        if (!$errors) {
+            $user = new User();
+            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+            $user->setEmail($email);
+            $user->setPassword($encodedPassword);
+
+            try {
+                $om->persist($user);
+                $om->flush();
+
+                return $this->json([
+                    'user' => $user
+                ]);
+            } catch (UniqueConstraintViolationException $e) {
+                $errors[] = "The email provided already has an account!";
+            } catch (\Exception $e) {
+                $errors[] = "Unable to save new user at this time.";
+            }
+        }
+
+        return $this->json([
+            'error' => $errors
+        ], 400);
+
+
     }
 }
